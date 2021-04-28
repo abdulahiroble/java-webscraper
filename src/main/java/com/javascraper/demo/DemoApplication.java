344543e928eabb08190ebf7ea058b3573c2055ad
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-28 20:04:26 +0200

    Add bufferedwriter

--- src/main/java/com/javascraper/demo/DemoApplication.java
+++ src/main/java/com/javascraper/demo/DemoApplication.java
@@ -1,47 +1,66 @@
 package com.javascraper.demo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.intenthq.gander.utils.JSoup;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) throws IOException {
-        /* Document d= Jsoup.connect("http://www.wikihow.com/wikiHowTo?search=Signal+Wifi").timeout(6000).get();
-        Elements ele=d.select("div#searchresults_list");
-        for (Element element : ele.select("div.result")) {
-            String img_url=element.select("div.result_thumb img").attr("src");
-            System.out.println(img_url);
-
-            String title=element.select("div.result_data a").text();
-            System.out.println(title);
-        } */
-
+        /*
+         * Document d=
+         * Jsoup.connect("http://www.wikihow.com/wikiHowTo?search=Signal+Wifi").timeout(
+         * 6000).get(); Elements ele=d.select("div#searchresults_list"); for (Element
+         * element : ele.select("div.result")) { String
+         * img_url=element.select("div.result_thumb img").attr("src");
+         * System.out.println(img_url);
+         * 
+         * String title=element.select("div.result_data a").text();
+         * System.out.println(title); }
+         */
 
         final String url = "https://thehub.io/jobs?countryCode=DK";
 
         try {
             final Document document = Jsoup.connect(url).get();
+            BufferedWriter writer = null;
+            String ticker = null;
 
             for (Element row : document.select("div.my-10")) {
 
                 if (row.select("div.my-10").text().equals("")) {
                     continue;
                 } else {
-                    final String ticker = row.select("div.my-10").text();
+                    writer = new BufferedWriter(new FileWriter("C:/Users/Abdul/Documents/test2.txt"));
+
+                    ticker = row.select("div.my-10").text();
+
+                    writer.write(document.toString());
+
                     System.out.println(ticker);
+
                 }
+
+                // try {
+                // writer = new BufferedWriter(new
+                // FileWriter("C:/Users/Abdul/Documents/test2.txt"));
+                // writer.write(document.toString());
+
+                // } catch (IOException e) {
+                // e.printStackTrace();
+                // }
             }
 
             // System.out.println(document.outerHtml());
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
